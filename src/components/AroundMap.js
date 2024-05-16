21f65ee2a00fd6b294dad72fb201e0138e788334
xuan <46474413+xuan-62@users.noreply.github.com>
2024-05-15 23:52:56 -0400

    migrated to latest version

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,54 +1,64 @@
-import React, {Component} from 'react';
-import { POS_KEY } from '../constants';
+import React, { Component } from "react";
+import { POS_KEY } from "../constants";
 
-import {
-    withScriptjs,
-    withGoogleMap,
-    GoogleMap,
-} from "react-google-maps";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
 class NormalAroundMap extends Component {
-    getMapRef = (mapInstance) => {
-        this.map = mapInstance;
-        window.map = mapInstance;
-    }
+  getMapRef = (mapInstance) => {
+    this.map = mapInstance;
+    window.map = mapInstance;
+  };
 
-    reloadMarker = () => {
-        const center = this.getCenter();
-        const radius = this.getRadius();
-        this.props.loadPostsByTopic(center, radius);
-    }
+  reloadMarker = () => {
+    const center = this.getCenter();
+    const radius = this.getRadius();
+    this.props.loadPostsByTopic(center, radius);
+  };
 
-    getCenter() {
-        const center = this.map.getCenter();
-        return { lat: center.lat(), lon: center.lng() };
-    }
+  getCenter() {
+    const center = this.map.getCenter();
+    return { lat: center.lat(), lon: center.lng() };
+  }
 
-    getRadius() {
-        const center = this.map.getCenter();
-        const bounds = this.map.getBounds();
-        if (center && bounds) {
-            const ne = bounds.getNorthEast();
-            const right = new window.google.maps.LatLng(center.lat(), ne.lng());
-            return 0.001 * window.google.maps.geometry.spherical.computeDistanceBetween(center, right);
-        }
+  getRadius() {
+    const center = this.map.getCenter();
+    const bounds = this.map.getBounds();
+    if (center && bounds) {
+      const ne = bounds.getNorthEast();
+      const right = new window.google.maps.LatLng(center.lat(), ne.lng());
+      return (
+        0.001 *
+        window.google.maps.geometry.spherical.computeDistanceBetween(
+          center,
+          right
+        )
+      );
     }
+  }
 
-    render() {
-        const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
-        return (
-            <GoogleMap
-                ref={this.getMapRef}
-                defaultZoom={11}
-                defaultCenter={{ lat, lng: lon }}
-                onDragEnd={this.reloadMarker}
-                onZoomChanged={this.reloadMarker}
-            >
-            </GoogleMap>
-        );
-    }
+  render() {
+    const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+    return (
+      <GoogleMap
+        onLoad={this.getMapRef}
+        defaultZoom={11}
+        defaultCenter={{ lat, lng: lon }}
+        onDragEnd={this.reloadMarker}
+        onZoomChanged={this.reloadMarker}
+      ></GoogleMap>
+    );
+  }
 }
 
-const AroundMap = withScriptjs(withGoogleMap(NormalAroundMap));
+const AroundMap = (props) => {
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: "YOUR_GOOGLE_MAPS_API_KEY", // Replace with your API key
+    libraries: ["geometry"], // Include necessary libraries
+  });
+
+  if (!isLoaded) return <div>Loading...</div>;
+
+  return <NormalAroundMap {...props} />;
+};
 
 export default AroundMap;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
     <BrowserRouter>
+      <StrictMode>
         <App />
+      </StrictMode>
     </BrowserRouter>
-    ,
-    document.getElementById('root')
+  </StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
